8ddcb3f2a087e2ced07c04423176f9b0a3f3388f
oscargonzalez <oscar.gonzalez.varela@gmail.com>
2014-07-28 00:47:02 +0200

    Fire routine done!

--- src/microgame/microgame.ino
+++ src/microgame/microgame.ino
@@ -24,6 +24,10 @@ Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 #define BATTESHIP_WIDTH    10
 #define BATTESHIP_HEIGHT   7
 #define BATTESHIP_FIRE_WIDTH    3  //Width of the shot
+#define BATTESHIP_FIRE_SPEED    3 
+
+// Splash logo 128x64
+const unsigned char PROGMEM splash_logo [] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 192, 48, 192, 0, 0, 0, 0, 15, 224, 0, 0, 0, 0, 0, 0, 0, 224, 112, 0, 0, 0, 0, 0, 56, 0, 0, 0, 0, 0, 0, 0, 0, 224, 240, 0, 0, 0, 0, 0, 96, 0, 0, 0, 0, 0, 0, 0, 1, 225, 224, 0, 0, 0, 0, 0, 192, 0, 0, 0, 0, 0, 0, 0, 1, 243, 96, 0, 0, 0, 0, 0, 192, 0, 0, 0, 0, 0, 0, 0, 1, 182, 97, 131, 240, 254, 7, 192, 192, 3, 248, 31, 248, 15, 128, 0, 3, 60, 195, 6, 1, 140, 28, 193, 135, 128, 28, 49, 156, 24, 192, 0, 3, 60, 195, 12, 1, 128, 48, 97, 129, 128, 12, 49, 140, 48, 192, 0, 3, 56, 195, 12, 1, 128, 48, 97, 129, 128, 12, 49, 140, 48, 192, 0, 6, 1, 134, 24, 3, 0, 96, 195, 3, 0, 24, 99, 24, 96, 192, 0, 6, 1, 134, 24, 3, 0, 96, 195, 3, 7, 248, 99, 24, 127, 192, 0, 6, 1, 134, 24, 3, 0, 96, 195, 3, 12, 24, 99, 24, 96, 0, 0, 12, 3, 12, 48, 6, 0, 193, 134, 6, 24, 48, 198, 48, 192, 0, 0, 12, 3, 12, 48, 6, 0, 193, 135, 6, 24, 48, 198, 48, 192, 0, 0, 12, 3, 12, 56, 6, 0, 231, 3, 6, 24, 48, 198, 48, 192, 0, 0, 24, 6, 24, 31, 12, 0, 124, 1, 252, 15, 225, 140, 96, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 28, 0, 0, 0, 14, 0, 0, 0, 32, 0, 0, 0, 0, 0, 0, 0, 34, 0, 0, 0, 17, 0, 0, 0, 32, 0, 0, 0, 0, 0, 0, 0, 65, 56, 227, 148, 32, 156, 243, 231, 39, 62, 0, 0, 0, 0, 0, 0, 65, 69, 20, 88, 32, 34, 136, 72, 168, 132, 0, 0, 0, 0, 0, 0, 65, 49, 3, 208, 35, 162, 136, 135, 175, 136, 0, 0, 0, 0, 0, 0, 65, 9, 4, 80, 32, 162, 136, 136, 168, 8, 0, 0, 0, 0, 0, 0, 34, 69, 20, 208, 17, 34, 137, 9, 168, 144, 0, 0, 0, 0, 0, 0, 28, 56, 227, 80, 14, 28, 139, 230, 167, 62, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 7, 28, 32, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 8, 162, 96, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 34, 104, 0, 162, 161, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 34, 152, 0, 162, 33, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 34, 136, 1, 34, 34, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31, 34, 136, 2, 34, 35, 224, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 166, 152, 4, 34, 32, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 154, 105, 15, 156, 32, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 240, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };    
   
 // Ship: 10x7
 const unsigned char PROGMEM ship [] = { 112, 0, 243, 0, 121, 128, 63, 192, 121, 128, 243, 0, 112, 0 };    
@@ -40,56 +44,28 @@ void setup()   {
   
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
-  // init done
-  
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
-  //display.display();
-  //delay(2000);
-
-  // Clear the buffer.
+ /*  
+  // Splash logo!
   display.clearDisplay();
-/*
-  // draw a single pixel
-  display.drawPixel(10, 10, WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-  display.display();
-
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Hello, world!");
-  display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.println(3.141592);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.print("0x"); display.println(0xDEADBEEF, HEX);
-  display.display();
+  display.drawBitmap(0, 0,  splash_logo, 128, 64, 1);
+  display.display();    
+  
+  for (int i=0 ; i<10 ; i++)
+  {
+    display.invertDisplay(true);
+    delay(100); 
+    display.invertDisplay(false);
+    delay(100);   
+  }
+  
   delay(2000);
-
-  // miniature bitmap display
-  display.clearDisplay();
-  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-  display.display();
-
-  // invert the display
-  display.invertDisplay(true);
-  delay(1000); 
-  display.invertDisplay(false);
-  delay(1000); 
-*/  
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.clearDisplay();
-  display.println("BattleShip");
-  display.display();  
   
+  display.startscrollleft(0x00, 0x0F);
+  delay(5000);
+  display.stopscroll();  
+*/  
   display.clearDisplay();
+  
 
 }
 
@@ -126,12 +102,15 @@ class Battleship
 
 Battleship::Battleship()
 {
+    int i;
+    
    _x=0;
    _y=0; 
    _lastx=0;
    _lasty=0;
    _velocity=2;
    fire_count=0;
+   for (i=0;i<MAX_FIRES;i++) { fires[i].x=0; }
 }
 
 void Battleship::moveRight()   { _lastx=_x; _x += _velocity; }
@@ -144,29 +123,47 @@ void Battleship::Fire()
 {
   if (fire_count < MAX_FIRES)
   {
-     BATTESHIP_HEIGHT 
-     fires[fire_count].x = BATTESHIP_WIDTH;
-     fires[fire_count].y = BATTESHIP_HEIGHT/2; // Divide in realtime in bad ;)
-     fire_count++;
+     for (int i=0 ; i<MAX_FIRES; i++)
+     {
+       if (fires[i].x == 0)
+       {
+         fires[i].x = _x + BATTESHIP_WIDTH;
+         fires[i].y = _y + (BATTESHIP_HEIGHT/2); // Divide in realtime in bad ;)
+         fire_count++;
+         break;
+       }
+     }
   }
 }
 
 void Battleship::update()
 {
   
-  int i;
+  int i,j;
   
   // Update position
   display.drawBitmap(_lastx, _lasty,  ship, 10, 7, 0);
   display.drawBitmap(_x, _y,  ship, 10, 7, 1);
 
   // Update fire
-  if (fire_count > 0)
+  for (i=0 ; i<MAX_FIRES ; i++)
   {
-    for (i=0 ; i<fire_count ; i++)
-    {
-      //BATTESHIP_FIRE_WIDTH      
-    }
+    //BATTESHIP_FIRE_WIDTH     
+    if (fires[i].x > 0)
+    { 
+      for (j=0 ; j<BATTESHIP_FIRE_WIDTH ; j++) { display.drawPixel(fires[i].x-j, fires[i].y, BLACK); } // Draw 3 pixels back for the full shot            
+      fires[i].x += BATTESHIP_FIRE_SPEED;
+      for (j=0 ; j<BATTESHIP_FIRE_WIDTH ; j++) { display.drawPixel(fires[i].x-j, fires[i].y, WHITE); } // Draw 3 pixels back for the full shot      
+      
+      //Shot is dead
+      if (fires[i].x > (128-BATTESHIP_FIRE_WIDTH))
+      {
+        for (j=0 ; j<BATTESHIP_FIRE_WIDTH ; j++) { display.drawPixel(fires[i].x-j, fires[i].y, BLACK); } // Erase shot
+        fires[i].x=0; 
+        fire_count--;
+      }
+      
+    }    
   }
 }
 
@@ -212,11 +209,19 @@ Battleship nave;
 Starfield stars;
 
 float inc=0;
+unsigned long last_fire=millis();
+
 void loop() {
   
   char x=0;
   
-  nave.setPosition(0, 32+sin(inc)*28);
+  nave.setPosition(0, 32+sin(inc)*20);
+  
+  if (millis()-last_fire > 400)
+  {
+     nave.Fire(); 
+     last_fire=millis();
+  }
   
   inc+=0.06;
       
@@ -227,13 +232,13 @@ void loop() {
     display.setTextColor(WHITE);
     display.setCursor(0,0);
     display.setTextColor(BLACK, WHITE); // 'inverted' text
-    display.println("SCORE: 0");    
+    display.println("PTS: 875");    
     
-    display.drawBitmap(64, 5, enemy1, 5, 5, 1);
-    display.drawBitmap(64, 15, enemy2, 5, 5, 1);
-    display.drawBitmap(64, 25, enemy3, 5, 5, 1);
-    display.drawBitmap(64, 35, enemy4, 5, 5, 1);
-    display.drawBitmap(64, 45, enemy5, 5, 5, 1);
+    display.drawBitmap(64, 10, enemy1, 5, 5, 1);
+    display.drawBitmap(64, 20, enemy2, 5, 5, 1);
+    display.drawBitmap(64, 30, enemy3, 5, 5, 1);
+    display.drawBitmap(64, 40, enemy4, 5, 5, 1);
+    display.drawBitmap(64, 50, enemy5, 5, 5, 1);
      
     display.display();    
   
